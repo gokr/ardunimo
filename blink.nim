61b4176f14ac87da4240322b2daeaa48f5efe936
Göran Krampe <goran.krampe@evothings.com>
2016-02-12 23:13:41 +0100

    Silly fix to blink.nim and some comments

--- blink.nim
+++ blink.nim
@@ -1,11 +1,15 @@
+# In nim we normally use import but wrapper/ardunimo.nim
+# needs to be included at the top of a Nim sketch.
 include ardunimo
 
+# ardunimo.nim defines two trivial templates, setup and loop
+# making it clean and convenient for this special case.
 setup:
+  # Nim is indentation based just like Python
   pinMode(13, OUTPUT)
 
 loop:
-  while true:
-    digitalWrite(13, LOW)
-    delay(300)
-    digitalWrite(13, HIGH)
-    delay(300)
+  digitalWrite(13, LOW)
+  delay(300)
+  digitalWrite(13, HIGH)
+  delay(300)
